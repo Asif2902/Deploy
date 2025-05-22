e724f50dbce19f56f6574bd0a2881580fac5f5c1
Asif <107271220+Asif2902@users.noreply.github.com>
2025-05-22 23:27:23 +0600

    update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,37 +1,36 @@
-require("@nomicfoundation/hardhat-toolbox"); // Includes ethers, waffle, etc.
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   solidity: {
-    version: "0.8.26", // Required for newer EVM targets like Shanghai + viaIR
+    version: "0.8.26",
     settings: {
       evmVersion: "shanghai",
       optimizer: {
         enabled: true,
-        runs: 2000000, // Higher for more optimized runtime gas
+        runs: 2000000,
       },
-      viaIR: true
-    }
+      viaIR: true,
+    },
   },
   networks: {
     hardhat: {
-      allowUnlimitedContractSize: true
+      allowUnlimitedContractSize: true,
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
-      allowUnlimitedContractSize: true
+      allowUnlimitedContractSize: true,
     },
     monad_testnet: {
       url: process.env.MONAD_TESTNET_RPC || "https://testnet-rpc.monad.xyz",
       chainId: 10143,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      allowUnlimitedContractSize: true
-    }
+      allowUnlimitedContractSize: true,
+    },
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
-  }
+    artifacts: "./artifacts",
+  },
 };
